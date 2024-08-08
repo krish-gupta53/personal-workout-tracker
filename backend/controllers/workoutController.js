e2a987c9ce0d005b79beea3900e35704733fbfb5
krish-gupta53 <krish.fig00@gmail.com>
2024-08-08 18:33:36 +0530

    almost done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,89 +1,103 @@
-const Workout=require('../models/workoutModel')
+const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
-//get all workouts
-const getWorkouts = async(req,res)=>{
-    const workouts = await Workout.find({}).sort({createdAt : -1})
-    res.status(200).json(workouts)
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+  const user_id = req.user._id
+
+  const workouts = await Workout.find({user_id}).sort({createdAt: -1})
+
+  res.status(200).json(workouts)
+}
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findById(id)
+
+  if (!workout) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+  
+  res.status(200).json(workout)
 }
 
 
-//get a single workout
-const getWorkout = async(req,res)=>{
-    const { id }= req.params
+// create new workout
+const createWorkout = async (req, res) => {
+  const {title, load, reps} = req.body
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such Workout'})
-    }
+  let emptyFields = []
 
-    const workout = await Workout.findById(id)
+  if(!title) {
+    emptyFields.push('title')
+  }
+  if(!load) {
+    emptyFields.push('load')
+  }
+  if(!reps) {
+    emptyFields.push('reps')
+  }
+  if(emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+  }
 
-    if(!workout){
-        return res.status(404).json({error: 'No such workout'})
-    }
+  // add doc to db
+  try {
+    const user_id = req.user._id
+    const workout = await Workout.create({title, load, reps, user_id})
     res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
-//create a new workout
-const createWorkout = async(req,res )=>{
-    const {title,load,reps} = req.body
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params
 
-    let emptyFields = []
-    if(!title){
-        emptyFields.push('title')
-    }
-    if(!load){
-        emptyFields.push('load')
-    }
-    if(!reps){
-        emptyFields.push('reps')
-    }
-    if(emptyFields.length >0){
-        return res.status(400).json({error : 'please fill in all the fields', emptyFields})
-    }
-    //add doc to db
-    try {
-        const workout = await Workout.create({title,load,reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndDelete({_id: id})
+
+  if (!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
 }
 
-//delete a workout
-const deleteWorkout= async(req,res)=>{
-    const { id }=req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such Workout'})
-    }
-    const workout = await Workout.findOneAndDelete({_id: id})
-    if(!workout){
-        return res.status(400).json({error: 'No such workout'})
-    }
+// update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params
 
-    res.status(200).json(workout)
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
 
-//update a workout
-const updateWorkout = async(req,res)=>{
-    const {id} = req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such Workout'})
-    }
-    const workout = await Workout.findOneAndUpdate({_id: id},{
-        ...req.body
-    })
-    if(!workout){
-        return res.status(400).json({error: 'No such workout'})
-    }
-    res.status(200).json(workout)
+  const workout = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
 
+  if (!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
 }
 
 
 module.exports = {
-    getWorkouts,
-    getWorkout,
-    createWorkout,
-    deleteWorkout,
-    updateWorkout
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
-}= require('../controllers/workoutController')
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
-const router=express.Router()
-// to get all workouts
-router.get('/' , getWorkouts)
-//get a single workout
-router.get('/:id',getWorkout)
+const router = express.Router()
 
-//post a new workout
-router.post('/',createWorkout)
-//delete a workout
-router.delete('/:id',deleteWorkout)
-//update a workout
-router.patch('/:id',updateWorkout)
+// require auth for all workout routes
+router.use(requireAuth)
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
 
 module.exports = router
